47643e7abfc4c05195f829a9ebf1b9bf008c6384
kirser5 <70627247+kirser5@users.noreply.github.com>
2021-12-16 20:52:58 +0300

    Delete ПР5/PR5 directory

--- ПР5/PR5/src/main/java/Practice23/config/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package Practice23.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableScheduling;
-
-@Configuration
-@EnableScheduling
-public class AppConfig {
-}
